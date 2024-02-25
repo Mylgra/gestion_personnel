5dc3e071a04e7126affe3f1ff1b72f64b9eff1ef
Tresor-Kasenda <scotttresor@gmail.com>
2024-02-25 16:49:31 +0100

    [checkbox] has checked trait

---
+++ app/View/TallFlex/Contracts/HasChecked.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\View\TallFlex\Contracts;
+
+trait HasChecked
+{
+    protected bool $checked = false;
+    
+    public function checked(bool $checked = true): static
+    {
+        $this->checked = $checked;
+        return $this;
+    }
+
+    public function getChecked()
+    {
+        return $this->evaluate($this->checked);
+    }
+}
