b3f74ab86508e4da26860f4eaa5c38f847b97966
Tresor-Kasenda <scotttresor@gmail.com>
2024-02-25 23:20:41 +0100

    [radio input] add radio input

---
+++ app/View/TallFlex/Forms/Inputs/RadioInput.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\View\TallFlex\Forms\Inputs;
+
+use App\View\TallFlex\Contracts\HasChecked;
+use App\View\TallFlex\Contracts\HasLabel;
+use App\View\TallFlex\Contracts\HasRequired;
+use App\View\TallFlex\Forms\GenerateForms;
+use Closure;
+use Illuminate\Contracts\Support\Htmlable;
+use Illuminate\View\View;
+use Livewire\Component;
+use Throwable;
+
+class RadioInput extends GenerateForms implements Htmlable
+{
+    use HasLabel;
+    use HasRequired;
+    use HasChecked;
+
+    protected string $uniqueId;
+
+    protected Component $livewire;
+
+    protected array $options = [];
+
+    protected bool $inline = false;
+
+    public function __construct(
+        public string $name
+    )
+    {
+        $this->uniqueId = uniqid('radio-' . $this->name, true);
+    }
+
+    public static function make(string $name): static
+    {
+        return new static($name);
+    }
+
+    public function getUniqueId(): string
+    {
+        return $this->evaluate($this->uniqueId);
+    }
+
+    public function evaluate(mixed $value)
+    {
+        if ($value instanceof Closure) {
+            return app()->call($value, [
+                'state' => $this->livewire->{$this->getName()},
+            ]);
+        }
+        return $value;
+    }
+
+    public function getName()
+    {
+        return $this->evaluate($this->name);
+    }
+
+    public function inline(): static
+    {
+        $this->inline = true;
+        return $this;
+    }
+
+    public function isInline(): bool
+    {
+        return $this->evaluate($this->inline);
+    }
+
+    public function boolean(): static
+    {
+        $this->options = ['Yes', 'No'];
+
+        return $this;
+    }
+
+    public function options(array $options): static
+    {
+        $this->options = $options;
+        return $this;
+    }
+
+    public function getOptions(): array
+    {
+        return $this->evaluate($this->options);
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function toHtml(): string
+    {
+        return $this->render()->render();
+    }
+
+    public function render(): View
+    {
+        return view('components.forms.radio', $this->extractPublicMethods());
+    }
+}
