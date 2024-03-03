f6478577323d45d124b43e6262c90561974a1608
Tresor-Kasenda <scotttresor@gmail.com>
2024-03-03 20:41:08 +0100

    update code review

--- routes/web.php
+++ routes/web.php
@@ -6,16 +6,25 @@ use App\Http\Controllers\ProfileController;
 use App\Livewire\Domains\Agents\CreateAgent;
 use App\Livewire\Domains\Agents\ListsAgents;
 use App\Livewire\Domains\Agents\ShowAgent;
+use App\Livewire\GestionService\Service\CreateService;
+use App\Livewire\GestionService\Service\ListService;
+use App\Livewire\GestionsService\Fonctions\ListsFonctions;
+use App\Livewire\GestionsService\Grades\ListsGrades;
 use App\Livewire\Home;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', Home::class)->name('home');
 
+Route::get('services', ListService::class)->name('services');
+Route::get('services/create', CreateService::class)->name('services.create');
+Route::get('grades', ListsGrades::class)->name('grades');
+Route::get('fonctions', ListsFonctions::class)->name('fonctions');
+
 Route::get('/agents', ListsAgents::class)->name('agents');
 Route::get('agents/create', CreateAgent::class)->name('create-agent');
 Route::get('agents/{agent}', ShowAgent::class)->name('show-agent');
 
-Route::get('/dashboard', fn () => view('dashboard'))->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', fn() => view('dashboard'))->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function (): void {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -23,4 +32,4 @@ Route::middleware('auth')->group(function (): void {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
