39108608c2e02a6a2785a087fb85fcc352be6e2b
Tresor-Kasenda <scotttresor@gmail.com>
2024-03-26 20:51:22 +0100

    [auth] add authentication - register - password reset - reset password - login and so more

--- app/Livewire/Pages/Auth/LoginComponent.php
+++ app/Livewire/Pages/Auth/LoginComponent.php
@@ -2,20 +2,27 @@
 
 namespace App\Livewire\Pages\Auth;
 
+use App\Providers\RouteServiceProvider;
+use Illuminate\Auth\Events\Lockout;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Str;
+use Illuminate\Validation\ValidationException;
 use Illuminate\View\View;
 use Livewire\Attributes\Layout;
 use Livewire\Attributes\Title;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
-use Tresorkasenda\Forms\Forms;
-use Tresorkasenda\Forms\Inputs\TextInput;
 
 #[Layout('layouts.guest')]
 #[Title('Login')]
 class LoginComponent extends Component
 {
-
+    #[Validate('required|string|email')]
     public string $email = '';
 
+    #[Validate('required|string')]
     public string $password = '';
 
     public function render(): View
@@ -23,27 +30,52 @@ class LoginComponent extends Component
         return view('livewire.pages.auth.login-component');
     }
 
-    public function component()
+    public function login(): void
     {
-        return Forms::make()
-            ->schema([
-                TextInput::make('email')
-                    ->label('Your Email')
-                    ->placeholder('Your email address')
-                    ->autofocus()
-                    ->autocomplete()
-                    ->email()
-                    ->required(),
-                TextInput::make('password')
-                    ->label('Your password')
-                    ->password()
-                    ->required()
-                    ->placeholder('Your password')
-            ]);
+        $this->validate();
+
+        $this->authentication();
+
+        Session::regenerate();
+
+        $this->redirectIntended(default: RouteServiceProvider::HOME, navigate: true);
     }
 
-    public function storeData()
+    protected function authentication(): void
     {
-        dd($this->all());
+        $this->ensureIsNotRateLimited();
+
+        if (!Auth::attempt($this->only(['email', 'password']))) {
+            RateLimiter::hit($this->throttleKey());
+
+            throw ValidationException::withMessages([
+                'form.email' => trans('auth.failed'),
+            ]);
+        }
+
+        RateLimiter::clear($this->throttleKey());
+    }
+
+    protected function ensureIsNotRateLimited(): void
+    {
+        if (!RateLimiter::tooManyAttempts($this->throttleKey(), 5)) {
+            return;
+        }
+
+        event(new Lockout(request()));
+
+        $seconds = RateLimiter::availableIn($this->throttleKey());
+
+        throw ValidationException::withMessages([
+            'email' => trans('auth.throttle', [
+                'seconds' => $seconds,
+                'minutes' => ceil($seconds / 60),
+            ]),
+        ]);
+    }
+
+    protected function throttleKey(): string
+    {
+        return Str::transliterate(Str::lower($this->email) . '|' . request()->ip());
     }
 }
--- resources/views/livewire/pages/auth/login-component.blade.php
+++ resources/views/livewire/pages/auth/login-component.blade.php
@@ -1,79 +1,87 @@
 <div class="nk-block nk-block-middle nk-auth-body">
     <div class="brand-logo pb-5">
-        <a href="" class="logo-link">
+        <a href="{{ route('home') }}" class="logo-link">
             <img
                 class="logo-light logo-img logo-img-lg"
-                src="./images/logo.png"
-                srcset="./images/logo2x.png 2x"
+                src="{{ asset('assets/images/logo.jpg') }}"
+                srcset="{{ asset('assets/images/logo.jpg') }} 2x"
                 alt="logo">
             <img
                 class="logo-dark logo-img logo-img-lg"
-                src="./images/logo-dark.png"
-                srcset="./images/logo-dark2x.png 2x"
+                src="{{ asset('assets/images/logo.jpg') }}"
+                srcset="{{ asset('assets/images/logo.jpg') }} 2x"
                 alt="logo-dark">
         </a>
     </div>
     <div class="nk-block-head">
         <div class="nk-block-head-content">
             <h5 class="nk-block-title">Sign-In</h5>
-            <div class="nk-block-des">
-                <p>Access the DashLite panel using your email and passcode.</p>
-            </div>
         </div>
     </div>
-    <form action="" method="post">
+    <form wire:submit="login">
         <div class="form-group">
-            <x-label
-                value="" ,
-                name=""
-            />
-            <x-text-input
-                wire:model=""
-                id=""
-                name=""
-                required
-                autofocus
-                autofocus=""
-                type="email"
-            />
+            <div class="form-label-group">
+                <x-label :value="__('Email')" for="email"/>
+            </div>
+            <div class="form-control-wrap">
+                <x-text-input
+                    wire:model="email"
+                    id="email"
+                    name="email"
+                    required
+                    autofocus
+                    autocomplete="email"
+                    type="email"
+                    class="form-control-lg"
+                    placeholder="Enter your email address"
+                />
+            </div>
+
+            <x-error :messages="$errors->get('email')" class="mt-1"/>
         </div>
 
         <div class="form-group">
-            <x-label
-                value="email" ,
-                name="email"
-            />
-            <x-text-input
-                wire:model="password"
-                id=""
-                name="password"
-                required
-                autofocus
-                autofocus="password"
-                type="password"
-            />
+            <div class="form-label-group">
+                <x-label :value="__('Password')" for="password"/>
+                <a
+                    class="link link-primary link-sm"
+                    tabindex="-1"
+                    href="{{ route('password.request') }}"
+                    wire:navigate
+                >Forgot password?</a>
+            </div>
+            <div class="form-control-wrap">
+                <x-text-input
+                    wire:model="password"
+                    id="password"
+                    name="password"
+                    required
+                    autofocus
+                    class="form-control-lg"
+                    autocomplete="password"
+                    type="password"
+                    placeholder="Enter your password"
+                />
+            </div>
+
+            <x-error :messages="$errors->get('password')" class="mt-1"/>
         </div>
         <x-primary-button
             type="submit"
-            class="btn-lg btn-outline-primary btn-block rounded"
-        >Sign in
+            class="btn-lg btn-outline-primary btn-dim btn-block rounded"
+            wire:loading.attr="disabled"
+        >
+            Sign in
         </x-primary-button>
     </form>
     <div class="form-note-s2 pt-4">
         New on our platform?
-        <a href="">Create an account</a>
+        <a href="{{ route('register') }}" wire:navigate>Create an account</a>
     </div>
     <div class="text-center pt-4 pb-3">
         <h6 class="overline-title overline-title-sap">
             <span>OR</span>
         </h6>
     </div>
-    <ul class="nav justify-center gx-4">
-        <li class="nav-item">
-            <a class="nav-link" href="#">Facebook</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Google</a>
-        </li>
-    </ul>
+    <x-auth-social/>
 </div>
